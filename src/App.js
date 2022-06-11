fb51793258917f56bc2647be60dce243c5bc78cc
amimbs <andymimbs91@gmail.com>
2022-06-11 00:29:22 -0400

    building out the forms

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link,
-  NavLink
+  Link
 } from "react-router-dom";
 import Home from './components/Home';
 import ErroPage from './components/ErrorPage';
 import Register from './components/Register';
 import Login from './components/Login';
-import Navigation from "./components/NavBar";
-import LeftLink from "./components/LeftLink";
+// import Navigation from "./components/NavBar";
+// import LeftLink from "./components/LeftLink";
 
 
 export default function App() {
 
   return (
     <Router>
-      <Navigation leftLink={<LeftLink />}/>
+      {/* <Navigation leftLink={<LeftLink />}/> */}
       {/* Routes tries to find the first and only componet that we specify */}
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/Login">Login</Link>
+        <Link to="/Register">Creat Account</Link>
+      </nav>
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Login' element={<Login />} />
@@ -30,4 +35,10 @@ export default function App() {
   );
 }
 
+// 11:14 / 32:58 Form Validation - Full Stack Web Development Course [4] | ReactJS, NodeJS, MySQL
+// starts using formik for form submission and validation
+
+
+
+// 25:45 / 38:34 Full Stack Web Development Course [8] | ReactJS, NodeJS, MySQL - Registration and Login
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function Register() {
+
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    userName: "",
+    password: ""
+  };
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
-    <div>Register</div>
+
+    <div className="register">
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Form className="formContainer">
+          <label>First Name</label>
+          <Field
+            id="inputFirstName"
+            name="firstName"
+            placeholder="Ex: John" />
+
+          <label>Last Name</label>
+          <Field
+            id="inputLastName"
+            name="lastName"
+            placeholder="Ex: Smith" />
+
+          <label>Email</label>
+          <Field
+            id="inputEmail"
+            name="email"
+            placeholder="Ex: jsmithy@hotmail.com" />
+
+          <label>User Name</label>
+          <Field
+            id="inputUserName"
+            name="userName"
+            placeholder="Ex: jsmith123" />
+
+          <label>Pasword</label>
+          <Field
+            autoComplete="off"
+            id="inputPassword"
+            name="password"
+            placeholder="Ex: jb007" />
+
+          <button type="submit">Create Account</button>
+        </Form>
+      </Formik>
+    </div>
   )
 }
 
