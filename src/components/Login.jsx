4e18b62c0fb30a80f70b30b7b9c952ecfc0e2988
amimbs <andymimbs91@gmail.com>
2022-06-11 13:44:46 -0400

    formik and Yup set up and function with registration pages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Login() {
+
+  const [userName, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = () => {
+    const data = {
+      userName: userName,
+      password: password
+    };
+
+    axios.post('http://localhost:3001/register/login', data).then((res) => {
+      console.log(res.data)
+    });
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button onClick={login}>Login</button>
+    </div>
   )
 }
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
 function Register() {
 
@@ -8,47 +10,107 @@ function Register() {
     lastName: "",
     email: "",
     userName: "",
-    password: ""
+    password: "",
+    confirmPassword: ""
   };
 
+  const validationSchema = Yup.object().shape({
+    firstName: Yup
+      .string()
+      .required('First Name is a required field'),
+    lastName: Yup
+      .string()
+      .required('Last name is a required field'),
+    email: Yup
+      .string()
+      .email('Not a valid email')
+      .required('Email is a required field'),
+    userName: Yup
+      .string()
+      .min(6, 'Username must be 6 characters')
+      .max(15, 'Username cannot exceed 15 characters')
+      .required('Username is a required field'),
+    password: Yup
+      .string()
+      .min(8, 'Password must be 6 characters')
+      .max(20, 'Password cannot exceed 20 characters')
+      .required('Password is a required field')
+      .matches(
+        /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+        "Password must contain at least 8 characters, one uppercase, one number and one special case character"
+      ),
+    confirmPassword: Yup
+      .string()
+      .required("Please confirm your password")
+      .when("password", {
+        is: password => (password && password.length > 0 ? true : false),
+        then: Yup.string().oneOf([Yup.ref("password")], "Password doesn't match")
+      })
+  })
+
   const onSubmit = (data) => {
-    console.log(data)
+    let user = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      userName: data.userName,
+      password: data.password
+    }
+
+    axios.post("http://localhost:3001/register", user).then((res) => {
+      console.log(res.request.status)
+    })
   }
 
   return (
 
     <div className="register">
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
         <Form className="formContainer">
-          <label>First Name</label>
+          <label> First Name </label>
+          <ErrorMessage name="firstName" component="span" />
           <Field
             id="inputFirstName"
             name="firstName"
             placeholder="Ex: John" />
 
-          <label>Last Name</label>
+          <label> Last Name </label>
+          <ErrorMessage name="lastName" component="span" />
           <Field
             id="inputLastName"
             name="lastName"
             placeholder="Ex: Smith" />
 
-          <label>Email</label>
+          <label> Email </label>
+          <ErrorMessage name="email" component="span" />
           <Field
             id="inputEmail"
             name="email"
             placeholder="Ex: jsmithy@hotmail.com" />
 
-          <label>User Name</label>
+          <label> Username </label>
+          <ErrorMessage name="userName" component="span" />
           <Field
             id="inputUserName"
             name="userName"
             placeholder="Ex: jsmith123" />
 
-          <label>Pasword</label>
+          <label> Password </label>
+          <ErrorMessage name="password" component="span" />
           <Field
             autoComplete="off"
+            type="password"
             id="inputPassword"
             name="password"
+            placeholder="Ex: jb007" />
+
+          <label> Confirm Password </label>
+          <ErrorMessage name="confirmPassword" component="span" />
+          <Field
+            autoComplete="off"
+            type="password"
+            id="inputConfirmPassword"
+            name="confirmPassword"
             placeholder="Ex: jb007" />
 
           <button type="submit">Create Account</button>
