e67de918e3e93e6249c1794c30dc48659c4f9286
amimbs <andymimbs91@gmail.com>
2022-06-23 21:03:56 -0400

    cors errors

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { toast } from "react-toastify";
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
